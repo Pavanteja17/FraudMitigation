acd08de2db665d776d7ecc7d2757b2bb88a531e1
Pavanteja17 <pavanvelagacherla123@gmail.com>
2024-06-21 10:28:56 +0530

    Integrated location access

--- app/src/main/java/com/example/fraudmitigation/ui/notifications/NotificationsFragment.kt
+++ app/src/main/java/com/example/fraudmitigation/ui/notifications/NotificationsFragment.kt
@@ -1,21 +1,37 @@
 package com.example.fraudmitigation.ui.notifications
 
+import android.Manifest
+import android.app.AlertDialog
+import android.content.pm.PackageManager
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.TextView
+import android.widget.Toast
+import androidx.core.content.ContextCompat
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
 import com.example.fraudmitigation.databinding.FragmentNotificationsBinding
+import com.google.android.gms.location.FusedLocationProviderClient
+import com.google.android.gms.location.LocationCallback
+import com.google.android.gms.location.LocationServices
 
 class NotificationsFragment : Fragment() {
 
     private var _binding: FragmentNotificationsBinding? = null
 
+    private lateinit var fusedLocationClient: FusedLocationProviderClient
+    private lateinit var locationCallback: LocationCallback
+
     // This property is only valid between onCreateView and
     // onDestroyView.
     private val binding get() = _binding!!
+
+    companion object {
+        private const val LOCATION_PERMISSION_REQUEST_CODE = 1
+        private const val BACKGROUND_LOCATION_PERMISSION_REQUEST_CODE = 2
+    }
 
     override fun onCreateView(
             inflater: LayoutInflater,
@@ -28,11 +44,76 @@ class NotificationsFragment : Fragment() {
         _binding = FragmentNotificationsBinding.inflate(inflater, container, false)
         val root: View = binding.root
 
+        fusedLocationClient = LocationServices.getFusedLocationProviderClient(requireActivity())
+
+
         val textView: TextView = binding.textNotifications
         notificationsViewModel.text.observe(viewLifecycleOwner) {
             textView.text = it
         }
+        showAuthenticationRequestDialogue();
         return root
+    }
+
+    private fun showAuthenticationRequestDialogue() {
+        val builder = AlertDialog.Builder(context)
+        builder.setMessage("To help mitigate fraudulent transactions, we request your access to your live location.")
+            .setPositiveButton("Yes") { dialog, id ->
+                // User clicked Yes button
+                requestLocationPermission()
+            }
+            .setNegativeButton("No") { dialog, id ->
+                // User clicked No button
+                Toast.makeText(context, "You chose No", Toast.LENGTH_SHORT).show()
+            }
+        builder.create().show()
+    }
+
+    private fun requestLocationPermission() {
+        if (ContextCompat.checkSelfPermission(requireContext(), Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            requestPermissions(arrayOf(Manifest.permission.ACCESS_FINE_LOCATION), LOCATION_PERMISSION_REQUEST_CODE)
+        } else {
+            // Check for background location permission if foreground permission is granted
+            if (ContextCompat.checkSelfPermission(requireContext(), Manifest.permission.ACCESS_BACKGROUND_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+                requestPermissions(arrayOf(Manifest.permission.ACCESS_BACKGROUND_LOCATION), BACKGROUND_LOCATION_PERMISSION_REQUEST_CODE)
+            } else {
+                //TODO:
+            }
+        }
+
+    }
+
+    @Deprecated("Deprecated in Java")
+    override fun onRequestPermissionsResult(requestCode: Int, permissions: Array<out String>, grantResults: IntArray) {
+        if (requestCode == LOCATION_PERMISSION_REQUEST_CODE) {
+            if ((grantResults.isNotEmpty() && grantResults[0] == PackageManager.PERMISSION_GRANTED)) {
+                // Permission granted
+                showConfirmationDialog("Location is granted while using the app");
+
+            } else {
+                // Permission denied
+                Toast.makeText(context, "Location permission denied", Toast.LENGTH_SHORT).show()
+            }
+        } else if (requestCode == BACKGROUND_LOCATION_PERMISSION_REQUEST_CODE) {
+            if ((grantResults.isNotEmpty() && grantResults[0] == PackageManager.PERMISSION_GRANTED)) {
+                // Background location permission granted
+                showConfirmationDialog("Thank you for granting the access. To mitigate fraudulent transactions, we will notify you if " +
+                        "transaction occurs in your account approximately 5 kilometers away from your live location");
+            } else {
+                // Permission denied
+                Toast.makeText(context, "Background location permission denied", Toast.LENGTH_SHORT).show()
+            }
+        }
+    }
+
+    private fun showConfirmationDialog(message: String) {
+        val builder = AlertDialog.Builder(context)
+        builder.setMessage(message)
+            .setPositiveButton("OK") { dialog, id ->
+                // User clicked OK button
+                dialog.dismiss()
+            }
+        builder.create().show()
     }
 
     override fun onDestroyView() {
--- app/src/main/java/com/example/fraudmitigation/ui/notifications/NotificationsViewModel.kt
+++ app/src/main/java/com/example/fraudmitigation/ui/notifications/NotificationsViewModel.kt
@@ -7,7 +7,7 @@ import androidx.lifecycle.ViewModel
 class NotificationsViewModel : ViewModel() {
 
     private val _text = MutableLiveData<String>().apply {
-        value = "This is notifications Fragment"
+        value = "Welcome to Fraud Mitigation notification service"
     }
     val text: LiveData<String> = _text
 }
